cc5de5da37de2656532c83c999110b4ee62c8364
Saif Arnab <saifornab@gmail.com>
2023-05-17 08:50:39 +0600

    new push

--- realtor/zillow.py
+++ realtor/zillow.py
@@ -1,49 +1,29 @@
+import csv
+import subprocess
 import sys
 import time
 from os.path import exists
-import csv
-from pandas import read_csv
-from user_agent import generate_user_agent
-import datetime
-import subprocess
 
-import requests
-from bs4 import BeautifulSoup
-from lxml import etree
-import undetected_chromedriver as uc
 from selenium import webdriver
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+from selenium.webdriver.support.wait import WebDriverWait
+from user_agent import generate_user_agent
 
 # install dependencies
-# subprocess.check_call(['pip', 'install', 'bs4'])
-# subprocess.check_call(['pip', 'install', 'requests'])
-# subprocess.check_call(['pip', 'install', 'lxml'])
+subprocess.check_call(['pip', 'install', 'user_agent'])
+subprocess.check_call(['pip', 'install', 'selenium'])
+
 
 def config_driver():
-    user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-    # user_agent = generate_user_agent()
     chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--allow-running-insecure-content')
     chrome_options.add_argument("--window-size=1920,1080")
-    # chrome_options.add_argument('--headless')
-    chrome_options.add_argument(f'user-agent={user_agent}')
-    driver = webdriver.Chrome(options=chrome_options)
-    return driver
-
-def config_uc_driver():
-    user_agent = generate_user_agent()
-    chrome_options = uc.ChromeOptions()
-    chrome_options.add_argument('--ignore-certificate-errors')
-    chrome_options.add_argument('--allow-running-insecure-content')
-    chrome_options.add_argument("--window-size=1920,1080")
-    # chrome_options.add_argument('--headless')
-    chrome_options.add_argument(f'user-agent={user_agent}')
-    driver = uc.Chrome(options=chrome_options)
-    return driver
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument(f'user-agent={generate_user_agent()}')
+    return webdriver.Chrome(options=chrome_options)
 
 
 def csv_file_init(file_name: str):
@@ -58,36 +38,55 @@ def csv_file_init(file_name: str):
         pass
 
 
-def write_csv(file_name: str, new_row: list) -> bool:
-    flag = True
-    add_hyperlink = f'=HYPERLINK("{new_row[12]}","{new_row[12]}")'
-    new_row[6] = add_hyperlink
+def check_existence(file_name: str, new_row: list) -> bool:
+
     with open(file_name, "r") as f:
         reader = csv.reader(f, delimiter=",")
         for i, line in enumerate(reader):
-            if line and new_row[12].strip() == line[12].strip():
-                flag = False
-                break
+            if line and new_row[3].strip() == line[3].strip():
+                return True
 
-    if flag is False:
-        print('This property already available ignoring..')
-        return False
+    return False
+
+
+def write_csv(file_name: str, new_row: list) -> bool:
+    new_row[12] = new_row[12].replace('%20', '')
+    add_hyperlink = f'=HYPERLINK("{new_row[12]}","{new_row[12]}")'
+    new_row[12] = add_hyperlink
 
     with open(file_name, 'a', newline='') as file:
         writer = csv.writer(file)
         writer.writerow(new_row)
-
+    print('--> New property added.')
     return True
 
 
-def get_address(filename: str) -> list:
+def get_properties_data(filename: str) -> list:
     try:
-        print(filename)
-        data = read_csv(filename)
-        return data['Address'].tolist()
+        file_data = open(filename, "r")
+        return list(csv.reader(file_data, delimiter=","))[1:]
     except Exception as e:
-        print('Address column not found in csv, exiting')
+        print('Failed to read csv')
         exit()
+
+
+def handle_captcha(driver):
+    try:
+        print('Handling captcha perimeter X')
+        open_window_elem = "#px-captcha"
+        x = int(driver.find_element(By.CSS_SELECTOR, open_window_elem).location['x'])
+        y = int(driver.find_element(By.CSS_SELECTOR, open_window_elem).location['y'])
+        width = int(driver.find_element(By.CSS_SELECTOR, open_window_elem).size['width'])
+        height = int(driver.find_element(By.CSS_SELECTOR, open_window_elem).size['height'])
+
+        action = ActionChains(driver)
+        action.move_by_offset(x + width / 2, y + height / 2)
+        action.click_and_hold()
+        time.sleep(1)
+        action.perform()
+        time.sleep(11)
+    except Exception as e:
+        pass
 
 
 def close_program(msg: str):
@@ -99,38 +98,59 @@ def close_program(msg: str):
 def scrapper(filename: str):
     print('---------------------------******----------------------')
     print('Script starts ...')
-
+    properties_data = get_properties_data(filename)
     zillow_filename = f'zillow_{filename}'
     csv_file_init(zillow_filename)
-    addresses = get_address(filename)
-
-    address_pointer = 0
-    while address_pointer < len(addresses):
-        address = addresses[address_pointer]
+    pointer = 0
+    while pointer < len(properties_data):
+        if check_existence(zillow_filename, properties_data[pointer]) is True:
+            print('Property already available, ignoring..')
+            pointer += 1
+            continue
+        address = properties_data[pointer][3]  # take address from the properties list
         driver = config_driver()
         driver.get(f'https://www.zillow.com/homes/{address}')
         time.sleep(1)
-        print(driver.current_url)
-        zillow_url, telephone = '', ''
         if 'captchaPerimeterX' in driver.current_url:
-            print('captcha issue generated')
-            address_pointer += 1
-            driver.find_element(By.XPATH, "//div[@id='px-captcha']")
-            time.sleep(10000)
+            handle_captcha(driver)
+            continue
 
-        elif '/b/' in driver.current_url:
+        try:
+            WebDriverWait(driver, 5).until(
+                EC.visibility_of_element_located(
+                    (By.XPATH, '//button[@class="ds-close-lightbox-icon hc-back-to-list"]')))
+        except Exception as e:
+            print(driver.current_url)
+            print('Zillow link not found')
+            row = properties_data[pointer]
+            row.append('')
+            row.append('')
+            write_csv(zillow_filename, row)
+            pointer += 1
+            driver.close()
+            time.sleep(1)
+
+        try:
+            zillow_url = driver.current_url
+            WebDriverWait(driver, 5).until(
+                EC.visibility_of_element_located(
+                    (By.XPATH, '//span[@class="RCFAgentPhoneDesktopText__phoneNumber"]')))
+            telephone = driver.find_element(By.XPATH, '//span[@class="RCFAgentPhoneDesktopText__phoneNumber"]').text
+            if telephone in [None, '', ' ']:
+                raise Exception('telephone number not found')
+        except Exception as e:
             try:
                 zillow_url = driver.current_url
-                WebDriverWait(driver, 20).until(
-                    EC.visibility_of_element_located(
-                        (By.XPATH, '//span[@class="RCFAgentPhoneDesktopText__phoneNumber"]')))
-
-                telephone = driver.find_element(By.XPATH, '//span[@class="RCFAgentPhoneDesktopText__phoneNumber"]').text
+                telephone = driver.find_element(By.XPATH, '//li[@class="ds-listing-agent-info-text"]').text
             except Exception as e:
+                zillow_url = driver.current_url
                 telephone = ''
 
-        address_pointer += 1
-        print(zillow_url, telephone)
+        row = properties_data[pointer]
+        row.append(zillow_url)
+        row.append(telephone)
+        write_csv(zillow_filename, row)
+        pointer += 1
         driver.close()
         time.sleep(1)
 
@@ -144,4 +164,4 @@ if __name__ == "__main__":
             scrapper(file)
     except Exception as e:
         print(e)
-        print('No filename define')
+        print('Something went wrong, please run the script again')
---
+++ the_numbers/movies.py
@@ -0,0 +1,125 @@
+print("Environment Creating......")
+import subprocess
+subprocess.check_call(['pip', 'install', 'pandas'])
+subprocess.check_call(['pip', 'install', 'selenium'])
+subprocess.check_call(['pip', 'install', 'webdriver_manager'])
+import pandas as pd
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument(
+    'user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.45 Safari/537.36')
+# options.add_argument("--window-size=1920,1080")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument("lang=en-GB")
+chrome_options.add_argument('--ignore-certificate-errors')
+# self.options.add_argument('--lang=en')
+chrome_options.add_argument('--allow-running-insecure-content')
+chrome_options.add_argument("--disable-extensions")
+chrome_options.add_argument("--proxy-server='direct://'")
+chrome_options.add_argument("--proxy-bypass-list=*")
+chrome_options.add_argument("--start-maximized")
+chrome_options.add_argument('--disable-gpu')
+chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--no-sandbox')
+# self.options.add_argument("--log-level=OFF")
+chrome_options.add_argument("--log-level=3")
+# driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
+driver.maximize_window()
+
+driver.get("https://www.the-numbers.com/movies/franchises")
+time.sleep(1)
+select = Select(driver.find_element(By.TAG_NAME, 'select'))
+select.select_by_visible_text('100')
+
+gc = pd.DataFrame()
+last_page = int(driver.find_elements(By.CSS_SELECTOR, "#franchise_overview_paginate > span > a:nth-child(n)")[-1].text)
+for b in range(last_page):
+    td = driver.find_elements(By.CSS_SELECTOR,
+                              "#franchise_overview > tbody > tr:nth-child(n) > td:nth-child(1) > b > a")
+    for a in range(len(td)):
+        fra_name = td[a].text
+        fra_u = td[a].get_attribute("href")
+        Dic = {'Franchise': fra_name, 'fra_link': fra_u}
+        gc = pd.concat([gc, pd.DataFrame([Dic])], ignore_index=True)
+        break
+
+    driver.find_element(By.CSS_SELECTOR, "#franchise_overview_next").click()
+    if b != 0:
+        print(f"extracted {len(gc)} franchises")
+    # if b==1:
+    #     break
+    break
+print(f"franchises extraction completed\n\n")
+time.sleep(2)
+print(f"Extracting Final Data.....")
+print(len(gc))
+gc2 = pd.DataFrame()
+for d in range(len(gc)):
+    try:
+        gc1 = pd.DataFrame()
+        driver.get(gc.fra_link[d])
+        time.sleep(2)
+        td = driver.find_elements(By.CSS_SELECTOR,
+                                  "#franchise_movies_overview > tbody > tr:nth-child(n) > td:nth-child(2) > b > a")
+        td1 = driver.find_elements(By.CSS_SELECTOR, "#franchise_movies_overview > tbody > tr:nth-child(n) > td.sorting_1")
+        for c in range(len(td)):
+            try:
+                if td[c].text.split(" ")[0] != "Untitled":
+                    try:
+                        date = td1[c].text.split(",")[-1]
+                    except:
+                        date = td1[c].text
+                    Dic = {'Franchise': gc.Franchise[d], 'Movie_title': td[c].text, 'Release_date': date}
+                    gc1 = pd.concat([gc1, pd.DataFrame([Dic])], ignore_index=True)
+            except:
+                continue
+
+        driver.get("https://www.imdb.com/?ref_=nv_home")
+
+        time.sleep(2)
+
+        for e in range(0, len(gc1)):
+            try:
+                time.sleep(2)
+                inp = driver.find_element(By.CSS_SELECTOR, "#suggestion-search")
+                txt = gc1.Movie_title[e] + " " + gc1.Release_date[e]
+                inp.send_keys(txt)
+                inp.send_keys(Keys.ENTER)
+                time.sleep(2)
+                try:
+                    mo_title = driver.find_element(By.CSS_SELECTOR,
+                                                   "#__next > main > div.ipc-page-content-container.ipc-page-content-container--full.sc-4f91839f-0.hmcKDt > div.ipc-page-content-container.ipc-page-content-container--center > section > div > div.ipc-page-grid__item.ipc-page-grid__item--span-2 > section:nth-child(3) > div.sc-17bafbdb-2.ffAEHI > ul > li:nth-child(1) > div.ipc-metadata-list-summary-item__c > div > a").text
+                    driver.find_element(By.CSS_SELECTOR,
+                                        "#__next > main > div.ipc-page-content-container.ipc-page-content-container--full.sc-4f91839f-0.hmcKDt > div.ipc-page-content-container.ipc-page-content-container--center > section > div > div.ipc-page-grid__item.ipc-page-grid__item--span-2 > section:nth-child(3) > div.sc-17bafbdb-2.ffAEHI > ul > li:nth-child(1) > div.ipc-metadata-list-summary-item__c > div > a").click()
+
+                    im = driver.current_url
+                    imdb = im.split("/")[-2]
+                    # print(imdb)
+                    Dic = {'Franchise': gc1.Franchise[e], 'Movie_title': gc1.Movie_title[e], 'Release_Year': gc1.Release_date[e],
+                           'IMDB_id': imdb}
+
+                except:
+                    Dic = {'Franchise': gc1.Franchise[e], 'Movie_title': mo_title,
+                           'Release_Year': gc1.Release_date[e],
+                           'IMDB_id': 'NA'}
+
+                gc2 = pd.concat([gc2, pd.DataFrame([Dic])], ignore_index=True)
+                if len(gc2) % 10 == 0:
+                    # gc2 = gc2.drop(gc2.columns[0], axis=1)
+                    gc2.loc[:, ['Franchise', 'Movie_title','Release_Year','IMDB_id']].to_excel("Output.xlsx", index=False)
+                    print(f"Extraxted {len(gc2)} Data")
+            except:
+                continue
+    except:
+        continue
+gc2.loc[:, ['Franchise', 'Movie_title','Release_Year','IMDB_id']].to_excel("Output.xlsx", index=False)
+print("Extraction Successfully Completed!!!!")
