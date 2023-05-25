415981c2b991c63fb36d433c2cdc003ee8cb8592
Saif Arnab <saifornab@gmail.com>
2023-05-25 15:59:48 +0600

    new word press work

--- streamyme/streamy.py
+++ streamyme/streamy.py
@@ -1,24 +1,28 @@
 import csv
+import datetime
+import random
 import subprocess
-import sys
 import time
 
+import pandas as pd
 import requests
 from selenium import webdriver
-from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from fake_useragent import UserAgent
 
 # define variables
+STREAMY_USER_ID = 'temp123'
+STREAMY_USER_PASS = '%$OV^@Kxc!44s3ez)KWQy#Kk'
 TELEGRAM_TOKEN = '6082996789:AAHGb1brvQfJzjrHy9H4WsCw-QRmESXXq7M'
-TELEGRAM_ID = '10016402776821'
+TELEGRAM_CHANNEL_ID = '10016402776821'
+
 
 # install dependencies
-# subprocess.check_call(['pip', 'install', 'user_agent'])
-# subprocess.check_call(['pip', 'install', 'selenium'])
-# subprocess.check_call(['pip', 'install', 'fake_useragent'])
+subprocess.check_call(['pip', 'install', 'pandas'])
+subprocess.check_call(['pip', 'install', 'selenium'])
+subprocess.check_call(['pip', 'install', 'requests'])
 
 
 def config_driver():
@@ -26,79 +30,167 @@ def config_driver():
     chrome_options.add_argument('--ignore-certificate-errors')
     chrome_options.add_argument('--allow-running-insecure-content')
     chrome_options.add_argument("--window-size=1920,1080")
-    # chrome_options.add_argument('--headless')
-    chrome_options.add_argument(f'user-agent={UserAgent().random}')
+    chrome_options.add_argument('--headless')
     return webdriver.Chrome(options=chrome_options)
 
 
-def streamy_login(driver):
-    driver.get('https://streamyme.com/wp-login.php')
-    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#user_login')))
-    driver.find_element(By.CSS_SELECTOR, '#user_login').send_keys('temp123')
-    driver.find_element(By.CSS_SELECTOR, '#user_pass').send_keys('%$OV^@Kxc!44s3ez)KWQy#Kk')
-    time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, '#wp-submit').click()
-    while driver.current_url != 'https://streamyme.com/wp-admin/':
-        continue
-    time.sleep(1)
+def csv_file_init(file_name: str):
+    try:
+        with open(file_name, 'x', newline='') as output_file:
+            writer = csv.writer(output_file)
+            writer.writerow(["LastPushedId"])
+            print('File created successfully.')
+    except FileExistsError:
+        pass
 
 
-def new_post(driver, title, content):
-    driver.get('https://streamyme.com/wp-admin/post-new.php')
-    WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CSS_SELECTOR, '#title')))
-    driver.find_element(By.CSS_SELECTOR, '#title').send_keys(title)
-    driver.find_element(By.CSS_SELECTOR, '#content-html').click()
-    time.sleep(1)
-    driver.find_element(By.CSS_SELECTOR, '#content').send_keys(content)
-    # driver.find_element(By.CSS_SELECTOR, '#publish').click()
-    time.sleep(10000)
+def get_last_telegram_id(file_name: str):
+    try:
+        df = pd.read_csv(file_name, sep=';')
+        return df['LastPushedId'][0]
+    except Exception as e:
+        return None
 
 
-def get_channel_messages(token, channel_id):
-    api_url = f"https://api.telegram.org/bot{token}/"
-    method = "getUpdates"  # Retrieves recent messages
-    params = {"chat_id": channel_id, "limit": 100}
+def write_csv(file_name: str, message_id: int):
+    try:
+        df = pd.read_csv(file_name)
+        df.loc[0, 'LastPushedId'] = str(message_id)
+        df.to_csv(file_name, index=False)
+    except Exception as e:
+        print(e)
 
-    response = requests.get(api_url + method, params=params)
-    data = response.json()
 
-    if response.status_code == 200 and data["ok"]:
-        messages = data["result"]
-        return messages
-    else:
-        print("Failed to retrieve channel messages.")
+def streamy_login(driver) -> bool:
+    try:
+        driver.get('https://streamyme.com/wp-login.php')
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.ID, 'user_login')))
+        driver.find_element(By.ID, 'user_login').send_keys(STREAMY_USER_ID)
+        driver.find_element(By.ID, 'user_pass').send_keys(STREAMY_USER_PASS)
+        time.sleep(1)
+        driver.find_element(By.ID, 'wp-submit').click()
+        while driver.current_url != 'https://streamyme.com/wp-admin/':
+            continue
+        time.sleep(1)
+        return True
+
+    except Exception as e:
+        return False
+
+
+def new_post(driver, title, content) -> bool:
+    try:
+        driver.get('https://streamyme.com/wp-admin/post-new.php')
+
+        # title
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.ID, 'title')))
+        driver.find_element(By.ID, 'title').send_keys(title)
+
+        # content
+        driver.find_element(By.ID, 'content-tmce').click()
+        time.sleep(1)
+        WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.ID, 'content_ifr')))
+        iframe = driver.find_element(By.ID, 'content_ifr')
+        driver.switch_to.frame(iframe)
+        time.sleep(1)
+
+        content = str(content).replace("'", "\\'")
+        element = driver.find_element(By.XPATH, '//body[@id="tinymce"]')
+        driver.execute_script("arguments[0].innerHTML = '{}'".format(content), element)
+        element.send_keys('.')
+        element.send_keys(Keys.BACKSPACE)
+
+        time.sleep(1)
+        driver.switch_to.default_content()
+        time.sleep(1)
+
+        # category
+        driver.find_element(By.ID, 'in-category-175').click()
+        time.sleep(1)
+
+        # publish
+        WebDriverWait(driver, 20).until(
+            EC.visibility_of_element_located((By.XPATH, '//input[@class="button button-primary button-large"]')))
+        driver.find_element(By.ID, 'publish').click()
+        time.sleep(5)
+
+        return True
+    except Exception as e:
+        return False
+
+
+def get_channel_messages() -> list:
+    try:
+        api_url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/getUpdates"
+        params = {"chat_id": TELEGRAM_CHANNEL_ID}
+        response = requests.get(api_url, params=params)
+        data = response.json()
+
+        if response.status_code == 200 and data["ok"]:
+            return data["result"]
+        else:
+            print("Failed to retrieve channel messages.")
+            return []
+    except Exception as e:
         return []
 
 
+def get_current_datetime():
+    return datetime.datetime.now().strftime("%d.%m.%Y / %H:%M %p")
+
+
+def waiting_time():
+    temp = random.randint(2, 4) * 60
+    print(f'No new post to publish. Wait {temp} seconds for next try.')
+    time.sleep(temp)
+
+
 def scrapper():
-    driver = config_driver()
-    streamy_login(driver)
-    text = """
-    Hey everyone!  Good morning! Just wanted to give you all a quick update on the current situation. Our amazing team has been working tirelessly throughout the night to bring back all the live channels, and I’m happy to report that we now have 60% of them back online.  The remaining channels will be restored gradually throughout the day.
+    print('Script starts running ...')
+    file_name = 'last_telegram_id_pointer.csv'
 
-Now, I know some of you might be wondering about the VOD section. Unfortunately, it’s not functioning at the moment.  Our first priority is to recover all the live channels, and once that’s done, we’ll start rebuilding the VOD section. Just a reminder, the entire data center where our data was stored had to be closed down, so we’re essentially starting from scratch and taking it one step at a time.
+    while True:
+        # create csv if not created
+        csv_file_init(file_name)
 
-A few customers have asked if any personal data was compromised during this process. I want to assure everyone that no personal data has been captured by the Netherlands task force or anyone else. The data center only stored the VOD section and managed the traffic for the live channels. So, there’s no need to worry about your personal information. Also, please note that we don’t store any personal user info except for email addresses and subscription lengths.  We take your privacy seriously, and that information is securely encrypted on a private server. No one can access your emails and subscription length info.
+        # get telegram recent posts
+        messages = get_channel_messages()
+        messages.reverse()
 
-We sincerely apologize for the inconvenience caused by the current service disruptions. To make it up to you, we’ll be adding 7 extra days to all subscriptions as compensation for the trouble. We truly appreciate your patience and understanding as we work hard to bring the service back to its full glory. 
+        if len(messages) < 1:
+            print('Not new post available in the channel')
+            waiting_time()
 
-Wishing you all an awesome day ahead! 
+        # get last publish telegram id
+        last_telegram_id = 0 if get_last_telegram_id(file_name) is None else get_last_telegram_id(file_name)
+        new_last_telegram_id = last_telegram_id
 
-the data should be presented in a nice and professional way, so customers easily can see the newest posts at the top, and then see older news under that
-    """
-    new_post(driver, 'Update – 24.05.2023', text)
+        # config driver
+        driver = config_driver()
+
+        # login to WordPress
+        if streamy_login(driver) is False:
+            print('Failed to login')
+            continue
+
+        # publish new post
+        for message in messages:
+            update_id = message['update_id']
+            if update_id > last_telegram_id:
+                try:
+                    content = message['channel_post']['text'].replace('\n', '\\n')
+                    title = f'Update - {get_current_datetime()}'
+                    if new_post(driver, title, content) is True:
+                        if update_id > new_last_telegram_id:
+                            new_last_telegram_id = update_id
+                        print(f'--> new post published.')
+                except Exception as e:
+                    continue
+
+        write_csv(file_name, new_last_telegram_id)
+        driver.close()
+        waiting_time()
 
 
 if __name__ == "__main__":
-    # scrapper()
-    # Replace with your Telegram token and channel ID
-    telegram_token = TELEGRAM_TOKEN
-    channel_id = TELEGRAM_ID
-
-    messages = get_channel_messages(telegram_token, channel_id)
-    print(len(messages))
-
-    for message in messages:
-        print('------------------------')
-        print(message)
-
+    scrapper()
