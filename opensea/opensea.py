fa92e7c218e53028896fb13865f0f76cb2bea5c3
Saifullah Mahbub <saifullah.mahbub@upaybd.com>
2023-07-02 00:55:46 +0600

    now

--- opensea/opensea.py
+++ opensea/opensea.py
@@ -3,8 +3,14 @@ import pillow_avif
 
 
 def convert_avif_to_png():
-    img = Image.open('img.avif')
-    img.save('img.png')
+    # img = Image.open('img.avif')
+    # rgb_im = im.convert('RGB')
+    # rgb_im.save('audacious.jpg')
+    # img.save('img.jpg')
+
+    im = Image.open("img.avif")
+    rgb_im = im.convert('RGB')
+    rgb_im.save('audacious.jpg')
 
 
 if __name__ == '__main__':
