f692f508485b77a1a7f3b3c60bf09118ca9e3ec2
Saif Arnab <saifornab@gmail.com>
2023-08-24 01:03:02 +0600

    gp

--- goal_profit/gp_v6.py
+++ goal_profit/gp_v6.py
@@ -1,25 +1,23 @@
 import csv
 import logging
 import os
-import random
 import subprocess
 import time
 from datetime import date
 
-import openpyxl
 import pandas as pd
+import xlwings as xw
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-import xlwings as xw
 
-
-# subprocess.check_call(['pip', 'install', 'selenium'])
-# subprocess.check_call(['pip', 'install', 'openpyxl'])
-# subprocess.check_call(['pip', 'install', 'pandas'])
+subprocess.check_call(['pip', 'install', 'selenium'])
+subprocess.check_call(['pip', 'install', 'openpyxl'])
+subprocess.check_call(['pip', 'install', 'pandas'])
+subprocess.check_call(['pip', 'install', 'xlwings'])
 
 # excel db path
-GAMEDB = 'scrap2.xlsx'
+GAMEDB = 'scrap.xlsx'
 
 # log format
 logging.basicConfig(
@@ -44,7 +42,7 @@ def config_driver(maximize_window: bool) -> webdriver.Chrome:
     chrome_options.add_argument('--disable-dev-shm-usage')
     chrome_options.add_argument('--no-sandbox')
     chrome_options.add_argument("--log-level=3")
-    driver = webdriver.Chrome(options=chrome_options)
+    driver = webdriver.Chrome(executable_path='chromedriver.exe', options=chrome_options)
     if maximize_window is True:
         driver.maximize_window()
     return driver
@@ -81,84 +79,11 @@ def save_csv_data(filename: str, row: list):
 
 
 def get_first_blank_row():
-    workbook = openpyxl.load_workbook(GAMEDB)
-    sheet = workbook['Scrape']
-    first_blank_row = sheet.max_row + 1  # Default to the first row if no data in the sheet
-    for row in sheet.iter_rows(min_row=1, max_row=sheet.max_row):
-        if all(cell.value is None for cell in row):
-            first_blank_row = row[0].row
-            break
-
-    return first_blank_row
-
-
-# Function to find the first blank row in a chunk
-def find_first_blank_row_in_chunk(chunk):
-    for row_num, row in enumerate(chunk, start=1):
-        print(row_num)
-        if all(cell is None for cell in row):
-            return row_num
-    return None
-
-
-def get_first_blank_row3():
     df = pd.read_excel(GAMEDB, engine="openpyxl")
     return df.shape[0] + 1
 
 
-def get_first_blank_row2():
-    print('hit 1')
-    # Load the Excel workbook
-    workbook = openpyxl.load_workbook(GAMEDB)
-    sheet = workbook['Scrape']
-    chunk_size = 1000
-    first_blank_row = 0
-    print('hit 2')
-    # Iterate through rows in chunks
-    for row_start in range(1, sheet.max_row + 1, chunk_size):
-        row_end = min(row_start + chunk_size - 1, sheet.max_row)
-        row_chunk = sheet.iter_rows(min_row=row_start, max_row=row_end, values_only=True)
-
-        blank_row_in_chunk = find_first_blank_row_in_chunk(row_chunk)
-        if blank_row_in_chunk is not None:
-            first_blank_row = row_start + blank_row_in_chunk - 1
-            break
-
-    return first_blank_row
-
-
 def insert_or_update_row(first_blank_row: int, new_data: list) -> bool:
-    df = pd.read_excel(GAMEDB, sheet_name='Scrape', skiprows=1)
-    # is_row_available = ((df['GameDate'] == new_data[0]) & (df['Game'] == new_data[1])).any()
-    # if is_row_available:
-    #     return False
-    workbook = openpyxl.load_workbook(GAMEDB)
-    sheet = workbook['Scrape']
-    start_row = first_blank_row  # Append after the last row in the sheet
-    start_col = 1  # Start from the first column (A)
-    for col_idx, value in enumerate(new_data, start=start_col):
-        sheet.cell(row=start_row, column=col_idx, value=value)
-    sheet.append(new_data)
-    workbook.save(GAMEDB)
-    workbook.close()
-    return True
-
-
-def insert_or_update_row2(first_blank_row: int, new_data: list) -> bool:
-    workbook = openpyxl.load_workbook(GAMEDB)
-    sheet = workbook["Scrape"]
-    data_exists = any(all(cell.value == value for cell, value in zip(row, new_data)) for row in sheet.iter_rows())
-
-    if not data_exists:
-        for col_num, value in enumerate(new_data, start=1):
-            sheet.cell(row=first_blank_row, column=col_num, value=value)
-        workbook.save(GAMEDB)
-        return True
-    else:
-        return False
-
-
-def insert_or_update_row3(first_blank_row: int, new_data: list) -> bool:
     app = xw.App(visible=False)  # Create an xlwings application object (hidden)
 
     try:
@@ -247,7 +172,7 @@ def scanner():
     if check_excel_file() is False:
         return
 
-    first_blank_row = get_first_blank_row3()
+    first_blank_row = get_first_blank_row()
     driver = config_driver(True)
 
     # iterate to extract game data
@@ -287,7 +212,7 @@ def scanner():
             # wait till 60s if no live games are available
             if len(table_elements) == 0:
                 logging.info('--> no live games are playing, waiting 60s for next try.')
-                time.sleep(5)
+                time.sleep(60)
                 continue
 
             # iterate each tr
@@ -306,10 +231,10 @@ def scanner():
                         game_time = 'N/A'
 
                     # if game time is not HT then return
-                    # if game_time != 'HT':
-                    #     tr_ind += 2
-                    #     logging.info('Waiting to detect HT')
-                    #     continue
+                    if game_time != 'HT':
+                        tr_ind += 2
+                        logging.info('Waiting to detect HT')
+                        continue
 
                     # extract required data from 1st tr
                     team1 = td_elements[1].text.index('(')
@@ -350,7 +275,7 @@ def scanner():
                     except Exception as exx:
                         home_da = 'N/A'
 
-                    logging.info('extracted required data from 1st tr')
+                    # logging.info('extracted required data from 1st tr')
 
                     # extract required data from 2nd tr
                     td_elements2 = tr_elements[tr_ind + 1].find_elements(By.XPATH, ".//td")
@@ -427,7 +352,7 @@ def scanner():
                     time.sleep(1)
 
                     # insert data to gamedb
-                    insert = insert_or_update_row3(first_blank_row,
+                    insert = insert_or_update_row(first_blank_row,
                                                   [current_date, game, home, away, draw, ht_home, ht_away, ht_draw,
                                                    home_on, home_off, home_da, away_on, away_off, away_da, ht_score])
                     tr_ind += 2
@@ -442,41 +367,10 @@ def scanner():
                         save_to_csv()
 
         except Exception as ex:
-            print(ex)
+            # print(ex)
             continue
-
-
-def scanner2():
-    # define 'driver' variable
-    if check_excel_file() is False:
-        return
-
-    first_blank_row = get_first_blank_row3()
-    game = "hello world"
-    current_date = date.today().strftime('%d/%m/%y')
-
-    # iterate to extract game data
-    while True:
-
-        # insert data to gamedb
-        insert = insert_or_update_row3(first_blank_row,
-                                      [current_date, game, 'home', '', '', 'ht_home', 'ht_away', 'ht_draw',
-                                       'home_on', 'home_off', 'home_da', 'away_on', 'away_off', 'away_da', 'ht_score'])
-        first_blank_row += 1
-
-        if insert is True:
-            logging.info(f'--> <{game, insert}> data is fetched and stored to gamedb!')
-            time.sleep(5)
-            save_to_csv()
-        else:
-            logging.info(f'--> <{game, insert}> data is already exist in gamedb!')
-            save_to_csv()
-
-        time.sleep(5)
 
 
 if __name__ == '__main__':
     logging.info('----------------- Script start running ... -----------------')
     scanner()
-    # v = get_first_blank_row3()
-    # print(v)
