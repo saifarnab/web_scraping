96201bcb72484605994db4d020a9cfc7b73024a4
Saifullah Mahbub <saifullah.mahbub@upaybd.com>
2023-09-23 12:52:16 +0600

    tennis

--- tennis24_v2/temp.py
+++ tennis24_v2/temp.py
@@ -1,102 +1,35 @@
-a =   ['https://www.tennis24.com/atp-singles/australian-open/',
-            'https://www.tennis24.com/atp-singles/bangkok/', 'https://www.tennis24.com/atp-singles/banja-luka/',
-            'https://www.tennis24.com/atp-singles/barcelona/', 'https://www.tennis24.com/atp-singles/basel/',
-            'https://www.tennis24.com/atp-singles/bastad/', 'https://www.tennis24.com/atp-singles/beijing/',
-            'https://www.tennis24.com/atp-singles/belgrade/', 'https://www.tennis24.com/atp-singles/belgrade-2/',
-            'https://www.tennis24.com/atp-singles/berlin/', 'https://www.tennis24.com/atp-singles/bermuda/',
-            'https://www.tennis24.com/atp-singles/birmingham/', 'https://www.tennis24.com/atp-singles/bogota/',
-            'https://www.tennis24.com/atp-singles/bologna/', 'https://www.tennis24.com/atp-singles/bolzano/',
-            'https://www.tennis24.com/atp-singles/bordeaux/', 'https://www.tennis24.com/atp-singles/boston/',
-            'https://www.tennis24.com/atp-singles/brasilia/', 'https://www.tennis24.com/atp-singles/brighton/',
-            'https://www.tennis24.com/atp-singles/brisbane/', 'https://www.tennis24.com/atp-singles/brussels/',
-            'https://www.tennis24.com/atp-singles/bucharest/', 'https://www.tennis24.com/atp-singles/budapest/',
-            'https://www.tennis24.com/atp-singles/buenos-aires/', 'https://www.tennis24.com/atp-singles/buzios/',
-            'https://www.tennis24.com/atp-singles/cagliari/', 'https://www.tennis24.com/atp-singles/casablanca/',
-            'https://www.tennis24.com/atp-singles/chengdu/', 'https://www.tennis24.com/atp-singles/chennai/',
-            'https://www.tennis24.com/atp-singles/chicago/', 'https://www.tennis24.com/atp-singles/cincinnati/',
-            'https://www.tennis24.com/atp-singles/cologne/', 'https://www.tennis24.com/atp-singles/cologne-2/',
-            'https://www.tennis24.com/atp-singles/copenhagen/', 'https://www.tennis24.com/atp-singles/cordoba/',
-            'https://www.tennis24.com/atp-singles/costa-do-sauipe/', 'https://www.tennis24.com/atp-singles/dallas/',
-            'https://www.tennis24.com/atp-singles/davis-cup-group-i/',
-            'https://www.tennis24.com/atp-singles/davis-cup-group-ii/',
-            'https://www.tennis24.com/atp-singles/davis-cup-group-iii/',
-            'https://www.tennis24.com/atp-singles/davis-cup-group-iv/',
-            'https://www.tennis24.com/atp-singles/davis-cup-group-v/',
-            'https://www.tennis24.com/atp-singles/davis-cup-world-group/',
-            'https://www.tennis24.com/atp-singles/davis-cup-world-group-i/',
-            'https://www.tennis24.com/atp-singles/davis-cup-world-group-ii/',
-            'https://www.tennis24.com/atp-singles/delray-beach/', 'https://www.tennis24.com/atp-singles/doha/',
-            'https://www.tennis24.com/atp-singles/dubai/', 'https://www.tennis24.com/atp-singles/dusseldorf/',
-            'https://www.tennis24.com/atp-singles/eastbourne/', 'https://www.tennis24.com/atp-singles/essen/',
-            'https://www.tennis24.com/atp-singles/estoril/', 'https://www.tennis24.com/atp-singles/finals-turin/',
-            'https://www.tennis24.com/atp-singles/florence/', 'https://www.tennis24.com/atp-singles/french-open/',
-            'https://www.tennis24.com/atp-singles/geneva/', 'https://www.tennis24.com/atp-singles/genoa/',
-            'https://www.tennis24.com/atp-singles/gijon/', 'https://www.tennis24.com/atp-singles/grand-slam-cup/',
-            'https://www.tennis24.com/atp-singles/gstaad/', 'https://www.tennis24.com/atp-singles/guaruja/',
-            'https://www.tennis24.com/atp-singles/halle/', 'https://www.tennis24.com/atp-singles/hamburg/',
-            'https://www.tennis24.com/atp-singles/hertogenbosch/',
-            'https://www.tennis24.com/atp-singles/ho-chi-minh-city/', 'https://www.tennis24.com/atp-singles/hong-kong/',
-            'https://www.tennis24.com/atp-singles/hopman-cup/', 'https://www.tennis24.com/atp-singles/houston/',
-            'https://www.tennis24.com/atp-singles/indianapolis/', 'https://www.tennis24.com/atp-singles/indian-wells/',
-            'https://www.tennis24.com/atp-singles/istanbul/', 'https://www.tennis24.com/atp-singles/itaparica/',
-            'https://www.tennis24.com/atp-singles/jakarta/', 'https://www.tennis24.com/atp-singles/johannesburg/',
-            'https://www.tennis24.com/atp-singles/kitzbuhel/', 'https://www.tennis24.com/atp-singles/kuala-lumpur/',
-            'https://www.tennis24.com/atp-singles/kuala-lumpur-2/', 'https://www.tennis24.com/atp-singles/las-vegas/',
-            'https://www.tennis24.com/atp-singles/laver-cup/', 'https://www.tennis24.com/atp-singles/london/',
-            'https://www.tennis24.com/atp-singles/long-island/', 'https://www.tennis24.com/atp-singles/los-angeles/',
-            'https://www.tennis24.com/atp-singles/los-cabos/', 'https://www.tennis24.com/atp-singles/lyon/',
-            'https://www.tennis24.com/atp-singles/lyon-2/', 'https://www.tennis24.com/atp-singles/maceio/',
-            'https://www.tennis24.com/atp-singles/madrid/', 'https://www.tennis24.com/atp-singles/madrid-2/',
-            'https://www.tennis24.com/atp-singles/mallorca/', 'https://www.tennis24.com/atp-singles/manchester/',
-            'https://www.tennis24.com/atp-singles/marbella/', 'https://www.tennis24.com/atp-singles/marrakech/',
-            'https://www.tennis24.com/atp-singles/marseille/',
-            'https://www.tennis24.com/atp-singles/mediterranean-games/',
-            'https://www.tennis24.com/atp-singles/melbourne-great-ocean-road-open/',
-            'https://www.tennis24.com/atp-singles/melbourne-murray-river-open/',
-            'https://www.tennis24.com/atp-singles/melbourne-summer-set/',
-            'https://www.tennis24.com/atp-singles/memphis/', 'https://www.tennis24.com/atp-singles/merano/',
-            'https://www.tennis24.com/atp-singles/metz/', 'https://www.tennis24.com/atp-singles/miami/',
-            'https://www.tennis24.com/atp-singles/milan/', 'https://www.tennis24.com/atp-singles/monte-carlo/',
-            'https://www.tennis24.com/atp-singles/montevideo/', 'https://www.tennis24.com/atp-singles/montpellier/',
-            'https://www.tennis24.com/atp-singles/montreal/', 'https://www.tennis24.com/atp-singles/moscow/',
-            'https://www.tennis24.com/atp-singles/mumbai/', 'https://www.tennis24.com/atp-singles/munich/',
-            'https://www.tennis24.com/atp-singles/napoli/', 'https://www.tennis24.com/atp-singles/new-haven/',
-            'https://www.tennis24.com/atp-singles/new-haven-2/', 'https://www.tennis24.com/atp-singles/newport/',
-            'https://www.tennis24.com/atp-singles/new-york/',
-            'https://www.tennis24.com/atp-singles/next-gen-finals-milan/', 'https://www.tennis24.com/atp-singles/nice/',
-            'https://www.tennis24.com/atp-singles/nottingham/', 'https://www.tennis24.com/atp-singles/oahu/',
-            'https://www.tennis24.com/atp-singles/olympic-games/', 'https://www.tennis24.com/atp-singles/oporto/',
-            'https://www.tennis24.com/atp-singles/orlando/', 'https://www.tennis24.com/atp-singles/osaka/',
-            'https://www.tennis24.com/atp-singles/ostrava/', 'https://www.tennis24.com/atp-singles/palermo/',
-            'https://www.tennis24.com/atp-singles/paris/', 'https://www.tennis24.com/atp-singles/parma/',
-            'https://www.tennis24.com/atp-singles/philadelphia/', 'https://www.tennis24.com/atp-singles/pinehurst/',
-            'https://www.tennis24.com/atp-singles/poertschach/', 'https://www.tennis24.com/atp-singles/prague/',
-            'https://www.tennis24.com/atp-singles/pune/', 'https://www.tennis24.com/atp-singles/quito/',
-            'https://www.tennis24.com/atp-singles/rio-de-janeiro/',
-            'https://www.tennis24.com/atp-singles/rio-de-janeiro-2/', 'https://www.tennis24.com/atp-singles/rome/',
-            'https://www.tennis24.com/atp-singles/rotterdam/', 'https://www.tennis24.com/atp-singles/san-diego/',
-            'https://www.tennis24.com/atp-singles/san-jose/', 'https://www.tennis24.com/atp-singles/san-marino/',
-            'https://www.tennis24.com/atp-singles/sanremo/', 'https://www.tennis24.com/atp-singles/santiago/',
-            'https://www.tennis24.com/atp-singles/sao-paulo/', 'https://www.tennis24.com/atp-singles/sao-paulo-2/',
-            'https://www.tennis24.com/atp-singles/sardinia/', 'https://www.tennis24.com/atp-singles/schenectady/',
-            'https://www.tennis24.com/atp-singles/scottsdale/', 'https://www.tennis24.com/atp-singles/seoul/',
-            'https://www.tennis24.com/atp-singles/shanghai/', 'https://www.tennis24.com/atp-singles/shanghai-2/',
-            'https://www.tennis24.com/atp-singles/shenzhen/', 'https://www.tennis24.com/atp-singles/singapore/',
-            'https://www.tennis24.com/atp-singles/sofia/', 'https://www.tennis24.com/atp-singles/sopot/',
-            'https://www.tennis24.com/atp-singles/split/', 'https://www.tennis24.com/atp-singles/stockholm/',
-            'https://www.tennis24.com/atp-singles/st-petersburg/', 'https://www.tennis24.com/atp-singles/stuttgart/',
-            'https://www.tennis24.com/atp-singles/stuttgart-1/', 'https://www.tennis24.com/atp-singles/sydney/',
-            'https://www.tennis24.com/atp-singles/sydney-2/', 'https://www.tennis24.com/atp-singles/taipei/',
-            'https://www.tennis24.com/atp-singles/tashkent/', 'https://www.tennis24.com/atp-singles/tel-aviv/',
-            'https://www.tennis24.com/atp-singles/tokyo/', 'https://www.tennis24.com/atp-singles/tokyo-2/',
-            'https://www.tennis24.com/atp-singles/toronto/', 'https://www.tennis24.com/atp-singles/toronto-2/',
-            'https://www.tennis24.com/atp-singles/toulouse/', 'https://www.tennis24.com/atp-singles/umag/',
-            'https://www.tennis24.com/atp-singles/united-cup/', 'https://www.tennis24.com/atp-singles/us-open/',
-            'https://www.tennis24.com/atp-singles/valencia/',
-            'https://www.tennis24.com/atp-singles/verizon-tennis-challenge/',
-            'https://www.tennis24.com/atp-singles/vienna/', 'https://www.tennis24.com/atp-singles/vina-del-mar/',
-            'https://www.tennis24.com/atp-singles/warsaw/', 'https://www.tennis24.com/atp-singles/washington/',
-            'https://www.tennis24.com/atp-singles/wellington/', 'https://www.tennis24.com/atp-singles/wembley/',
-            'https://www.tennis24.com/atp-singles/wimbledon/', 'https://www.tennis24.com/atp-singles/winston-salem/',
-            'https://www.tennis24.com/atp-singles/zagreb/', 'https://www.tennis24.com/atp-singles/zaragoza/',
-            'https://www.tennis24.com/atp-singles/zhuhai/']
+import openpyxl
+import pandas as pd
+
+
+def check_data_exists():
+    filename = 'files/atp_1.xlsx'
+    workbook = openpyxl.load_workbook(filename)
+    sheet = workbook.active
+    for row in sheet.iter_rows(values_only=True):
+        print(row[3])
+
+
+def new_line_parser():
+    file_path = 'files/wta_1.xlsx'  # Replace with the actual path to your Excel file
+    df = pd.read_excel(file_path)
+
+    # Function to remove newlines and handle NaN values
+    def process_cell(cell_value):
+
+        if pd.isna(cell_value):  # Check if the value is NaN
+            return ''  # Replace NaN with an empty string
+        else:
+            return str(cell_value).replace('\n', '')
+
+    # Apply the process_cell function to each cell in the DataFrame
+    df = df.applymap(process_cell)
+
+    # Save the processed DataFrame to a new Excel file
+    output_file_path = 'files/new_wtp.xlsx'  # Replace with the desired output file path
+    df.to_excel(output_file_path, index=False)
+
+    print('Data processed and saved to', output_file_path)
+
+
+new_line_parser()
